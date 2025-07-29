2088447296f83d5bbd4957da795c37470020ad7f
whyssarah <mariyam.sarah35@gmail.com>
2025-07-29 20:22:07 +0530

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,6 @@
+#A program to show if a person is eligible to vote in accordance to the voting laws in India.
+a = int(input("Enter your age:"))
+if a>= 18:
+    print("The person is eligible to vote. Please cast your vote.")
+else:
+    print("The person is not eligible to vote yet.")
